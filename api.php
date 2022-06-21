e9593713232e815012d5995043005eb2dc003bd4
AidilAhda <76725012+AidilAhda@users.noreply.github.com>
2022-06-21 17:04:39 +0700

    add header acces control

--- api.php
+++ api.php
@@ -1,4 +1,6 @@
 <?php
+header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Headers:*');
 $conn = new mysqli("localhost", "root", "", "uas");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
